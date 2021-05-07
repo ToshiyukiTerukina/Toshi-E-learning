ff34e8510e46766aeaa7ed56b8431975ee38a993
ToshiyukiTerukina <toshiyuki.terukina@bizmates.jp>
2021-05-07 17:16:38 +0900

    [add] make all migrations

---
+++ database/migrations/2021_05_07_073614_create_activities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('lesson_id')->constrained('lessons');
+            $table->foreignId('relationship_id')->constrained('relationships');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
