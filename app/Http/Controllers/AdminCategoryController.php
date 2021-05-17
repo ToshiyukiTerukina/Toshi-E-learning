1ed59b48af23fa8d373f5feca10cb4d3913408c4
ToshiyukiTerukina <toshiyuki.terukina@bizmates.jp>
2021-05-17 16:53:15 +0900

    [add] Make Admin dashboard.

---
+++ app/Http/Controllers/AdminCategoryController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class AdminCategoryController extends Controller
+{
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+
+    public function index()
+    {
+        //categoriを全て取得して表示
+        $categories = $this->category->getAllCategories();
+        return view('admin/category/index', compact('categories'));
+
+    }
+
+    public function showCreateForm()
+    {
+        return view('admin/category/create');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, $this->category::$rules);
+
+        if ($this->category->createCategory($request)) {
+            return redirect()->route('admin.dashboard')->with('success', 'Category created successfully');
+        }
+
+    }
+
+    public function edit(Request $request)
+    {
+        if (is_null($this->category->getCategoryById($request->id))) {
+            return redirect()->route('admin.dashboard')->with('error', 'does not exist');
+        }
+        $category = $this->category->getCategoryById($request->id);
+        return view('admin/category/edit', compact('category'));
+    }
+
+    public function update(Request $request)
+    {
+        $this->validate($request, $this->category::$rules);
+
+        if (is_null($this->category->getCategoryById($request->id))) {
+            return redirect()->route('admin.dashboard')->with('error', 'does not exist');
+        }
+
+        if ($this->category->updateCategory($request)) {
+            return redirect()->route('admin.dashboard')->with('success', 'Category updated successfully');
+        }
+
+    }
+
+    public function delete(Request $request)
+    {
+        if (is_null($this->category->getCategoryById($request->id))) {
+            return redirect()->route('admin.dashboard')->with('error', 'does not exist');
+        }
+
+        if ($this->category->deleteCategory($request)) {
+            return redirect()->route('admin.dashboard')->with('success', 'Category deleted successfully');
+        }
+    }
+
+}
